e6dc59d69a58783a53d19460812bcffccff228b8
Zhe Yu <zcabzyu@ucl.ac.uk>
2025-05-28 20:24:57 +0800

    refactor(cli): wrap internal errors into rpc error

--- src/vectorcode/lsp_main.py
+++ src/vectorcode/lsp_main.py
@@ -6,7 +6,11 @@ import sys
 import time
 import uuid
 
-from pygls.exceptions import JsonRpcInvalidRequest
+from pygls.exceptions import (
+    JsonRpcException,
+    JsonRpcInternalError,
+    JsonRpcInvalidRequest,
+)
 
 try:  # pragma: nocover
     from lsprotocol import types
@@ -68,89 +72,101 @@ server: LanguageServer = LanguageServer(name="vectorcode-server", version=__vers
 
 @server.command("vectorcode")
 async def execute_command(ls: LanguageServer, args: list[str]):
-    global DEFAULT_PROJECT_ROOT
-    start_time = time.time()
-    logger.info("Received command arguments: %s", args)
-    parsed_args = await parse_cli_args(args)
-    logger.info("Parsed command arguments: %s", parsed_args)
-    if parsed_args.action not in {CliAction.query, CliAction.ls}:
-        error_message = f"Unsupported vectorcode subcommand: {str(parsed_args.action)}"
-        logger.error(
-            error_message,
-        )
-        raise JsonRpcInvalidRequest(error_message)
-    if parsed_args.project_root is None:
-        if DEFAULT_PROJECT_ROOT is not None:
-            parsed_args.project_root = DEFAULT_PROJECT_ROOT
-            logger.warning("Using DEFAULT_PROJECT_ROOT: %s", DEFAULT_PROJECT_ROOT)
-    elif DEFAULT_PROJECT_ROOT is None:
-        logger.warning("Updating DEFAULT_PROJECT_ROOT to %s", parsed_args.project_root)
-        DEFAULT_PROJECT_ROOT = str(parsed_args.project_root)
-
-    if parsed_args.project_root is not None:
-        parsed_args.project_root = os.path.abspath(str(parsed_args.project_root))
-        await make_caches(parsed_args.project_root)
-        final_configs = await cached_project_configs[
-            parsed_args.project_root
-        ].merge_from(parsed_args)
-        final_configs.pipe = True
-        client = await get_client(final_configs)
-        collection = await get_collection(
-            client=client,
-            configs=final_configs,
-            make_if_missing=final_configs.action in {CliAction.vectorise},
-        )
-    else:
-        final_configs = parsed_args
-        client = await get_client(parsed_args)
-        collection = None
-    logger.info("Merged final configs: %s", final_configs)
-    progress_token = str(uuid.uuid4())
-
-    await ls.progress.create_async(progress_token)
-    match final_configs.action:
-        case CliAction.query:
-            ls.progress.begin(
-                progress_token,
-                types.WorkDoneProgressBegin(
-                    "VectorCode",
-                    message=f"Querying {cleanup_path(str(final_configs.project_root))}",
-                ),
+    try:
+        global DEFAULT_PROJECT_ROOT
+        start_time = time.time()
+        logger.info("Received command arguments: %s", args)
+        parsed_args = await parse_cli_args(args)
+        logger.info("Parsed command arguments: %s", parsed_args)
+        if parsed_args.action not in {CliAction.query, CliAction.ls}:
+            error_message = (
+                f"Unsupported vectorcode subcommand: {str(parsed_args.action)}"
             )
-            final_results = []
-            try:
-                if collection is None:
-                    print("Please specify a project to search in.", file=sys.stderr)
-                else:
-                    final_results.extend(
-                        await build_query_results(collection, final_configs)
+            logger.error(
+                error_message,
+            )
+            raise JsonRpcInvalidRequest(error_message)
+        if parsed_args.project_root is None:
+            if DEFAULT_PROJECT_ROOT is not None:
+                parsed_args.project_root = DEFAULT_PROJECT_ROOT
+                logger.warning("Using DEFAULT_PROJECT_ROOT: %s", DEFAULT_PROJECT_ROOT)
+        elif DEFAULT_PROJECT_ROOT is None:
+            logger.warning(
+                "Updating DEFAULT_PROJECT_ROOT to %s", parsed_args.project_root
+            )
+            DEFAULT_PROJECT_ROOT = str(parsed_args.project_root)
+
+        if parsed_args.project_root is not None:
+            parsed_args.project_root = os.path.abspath(str(parsed_args.project_root))
+            await make_caches(parsed_args.project_root)
+            final_configs = await cached_project_configs[
+                parsed_args.project_root
+            ].merge_from(parsed_args)
+            final_configs.pipe = True
+            client = await get_client(final_configs)
+            collection = await get_collection(
+                client=client,
+                configs=final_configs,
+                make_if_missing=final_configs.action in {CliAction.vectorise},
+            )
+        else:
+            final_configs = parsed_args
+            client = await get_client(parsed_args)
+            collection = None
+        logger.info("Merged final configs: %s", final_configs)
+        progress_token = str(uuid.uuid4())
+
+        await ls.progress.create_async(progress_token)
+        match final_configs.action:
+            case CliAction.query:
+                ls.progress.begin(
+                    progress_token,
+                    types.WorkDoneProgressBegin(
+                        "VectorCode",
+                        message=f"Querying {cleanup_path(str(final_configs.project_root))}",
+                    ),
+                )
+                final_results = []
+                try:
+                    if collection is None:
+                        print("Please specify a project to search in.", file=sys.stderr)
+                    else:
+                        final_results.extend(
+                            await build_query_results(collection, final_configs)
+                        )
+                finally:
+                    log_message = f"Retrieved {len(final_results)} result{'s' if len(final_results) > 1 else ''} in {round(time.time() - start_time, 2)}s."
+                    ls.progress.end(
+                        progress_token,
+                        types.WorkDoneProgressEnd(message=log_message),
                     )
-            finally:
-                log_message = f"Retrieved {len(final_results)} result{'s' if len(final_results) > 1 else ''} in {round(time.time() - start_time, 2)}s."
-                ls.progress.end(
+                    logger.info(log_message)
+                return final_results
+            case CliAction.ls:
+                ls.progress.begin(
                     progress_token,
-                    types.WorkDoneProgressEnd(message=log_message),
+                    types.WorkDoneProgressBegin(
+                        "VectorCode",
+                        message="Looking for other projects indexed by VectorCode",
+                    ),
                 )
-                logger.info(log_message)
-            return final_results
-        case CliAction.ls:
-            ls.progress.begin(
-                progress_token,
-                types.WorkDoneProgressBegin(
-                    "VectorCode",
-                    message="Looking for other projects indexed by VectorCode",
-                ),
-            )
-            projects: list[dict] = []
-            try:
-                projects.extend(await get_collection_list(client))
-            finally:
-                ls.progress.end(
-                    progress_token,
-                    types.WorkDoneProgressEnd(message="List retrieved."),
-                )
-                logger.info(f"Retrieved {len(projects)} project(s).")
-            return projects
+                projects: list[dict] = []
+                try:
+                    projects.extend(await get_collection_list(client))
+                finally:
+                    ls.progress.end(
+                        progress_token,
+                        types.WorkDoneProgressEnd(message="List retrieved."),
+                    )
+                    logger.info(f"Retrieved {len(projects)} project(s).")
+                return projects
+    except Exception as e:
+        if isinstance(e, JsonRpcException):
+            # pygls exception. raise it as is.
+            raise
+        else:  # pragma: nocover
+            # wrap non-pygls errors for error codes.
+            raise JsonRpcInternalError(e.__traceback__) from e
 
 
 async def lsp_start() -> int:
